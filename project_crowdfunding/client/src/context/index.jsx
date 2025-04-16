5dd7687412a388cba17bb1e7feab4812ddce284f
nhat.nm <nhat.nm-it@open-dev.io>
2025-04-16 21:12:09 +0700

    edit hardhat config

--- project_crowdfunding/client/src/context/index.jsx
+++ project_crowdfunding/client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x70997970C51812dc3A010C7d01b50e0d17dc79C8');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
@@ -25,7 +25,6 @@ export const StateContextProvider = ({ children }) => {
 					form.image,
 				],
 			});
-
       console.log("contract call success", data)
     } catch (error) {
       console.log("contract call failure", error)
--- project_crowdfunding/web3/hardhat.config.js
+++ project_crowdfunding/web3/hardhat.config.js
@@ -2,13 +2,22 @@
 module.exports = {
   solidity: {
     version: '0.8.9',
-    // defaultNetwork: 'goerli',
+    defaultNetwork: 'localhost',
     networks: {
       hardhat: {},
       goerli: {
         url: 'https://rpc.ankr.com/eth_goerli',
         accounts: [`0x${process.env.PRIVATE_KEY}`]
+      },
+      sepolia: {
+        url: "https://sepolia.infura.io/v3/<key>",
+        accounts: [privateKey1, privateKey2, ...]
       }
+  
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainID: 1337,
     },
     settings: {
       optimizer: {
