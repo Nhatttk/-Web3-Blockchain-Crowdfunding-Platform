a965e9a6a53b7a142918f595b8710f27395ff062
nhat.nm <nhat.nm-it@open-dev.io>
2025-04-19 14:04:49 +0700

    update web3 config

--- project_crowdfunding/web3/hardhat.config.js
+++ project_crowdfunding/web3/hardhat.config.js
@@ -1,24 +1,10 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: '0.8.9',
-    defaultNetwork: 'localhost',
-    networks: {
-      hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
-      },
-      sepolia: {
-        url: "https://sepolia.infura.io/v3/<key>",
-        accounts: [privateKey1, privateKey2, ...]
-      }
-  
-    },
-    localhost: {
-      url: "http://127.0.0.1:8545",
-      chainID: 1337,
-    },
     settings: {
       optimizer: {
         enabled: true,
@@ -26,4 +12,20 @@ module.exports = {
       },
     },
   },
+  defaultNetwork: 'localhost',
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+    // goerli: {
+    //   url: 'https://rpc.ankr.com/eth_goerli',
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`]
+    // },
+    // sepolia: {
+    //   url: "https://sepolia.infura.io/v3/<key>",
+    //   accounts: [`0x${process.env.PRIVATE_KEY}`]
+    // }
+  }
 };
